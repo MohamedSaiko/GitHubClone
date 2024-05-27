de59a7b0ccdf3be48246c8eb92a82c5777bc7895
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-27 07:28:51 +0200

    Initial Commit

---
+++ GitHubCloneUITests/GitHubCloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHubCloneUITestsLaunchTests.swift
+//  GitHubCloneUITests
+//
+//  Created by Mohamed Sayed on 27.05.24.
+//
+
+import XCTest
+
+final class GitHubCloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
