82ef779733b39dd0a3d98e6298fd2a7a9d756331
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 14:37:43 +0200

    Create Repository Cell

--- GitHubClone/RepositoriesView/RepositoriesCell/RepositoryCell.swift
+++ GitHubClone/RepositoriesView/RepositoriesCell/RepositoryCell.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class RepositoryCell: UITableViewCell {
+    @IBOutlet weak var repositoryNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var licenseLabel: UILabel!
     
-    
-    
-    
-    func configure(fromRepository: Repository) {
+    func configure(fromRepository repository: Repository) {
+        repositoryNameLabel.text = repository.name
         
+        guard let description = repository.description, let license = repository.license else {
+            descriptionLabel.text = "No Description"
+            licenseLabel.text = "No License"
+            return
+        }
+        
+        descriptionLabel.text = "Description:\n" + description
+        licenseLabel.text = "License: \(license.name)"
     }
 }
