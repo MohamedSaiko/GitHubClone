fde3adae92093fc2508baf7ab90bf93fb00399a0
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 08:01:17 +0200

    create UserCellViewModel

--- GitHubClone/UsersView/UsersCell/UserCellViewModel.swift
+++ GitHubClone/UsersView/UsersCell/UserCellViewModel.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+final class UserCellViewModel {
+    private let imageDownloader: ImageDownloader
+    
+    init(imageDownloader: ImageDownloader) {
+        self.imageDownloader = imageDownloader
+    }
+    
+    func downloadImage(fromURL url: URL, completion: @escaping (Data) -> Void) {
+        imageDownloader.download(fromURL: url) { result in
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
