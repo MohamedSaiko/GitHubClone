6a5fe43f984d0ff8a84ec0394353a0df89aa1bf9
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 16:27:32 +0200

    Create ForkCell

--- GitHubClone/ForksView/ForksCell/ForkCell.swift
+++ GitHubClone/ForksView/ForksCell/ForkCell.swift
@@ -8,16 +8,9 @@
 import UIKit
 
 class ForkCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var forkName: UILabel!
+    
+    func configure(fromFork fork: Fork) {
+        forkName.text = fork.owner.login
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
