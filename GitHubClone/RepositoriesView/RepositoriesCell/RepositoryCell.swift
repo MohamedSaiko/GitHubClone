874e974ff264e6af24d3ad30a02998d710677268
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 14:35:29 +0200

    Create RepositoriesViewController in Storyboard

---
+++ GitHubClone/RepositoriesView/RepositoriesCell/RepositoryCell.swift
@@ -0,0 +1,18 @@
+//
+//  RepositoriyCell.swift
+//  GitHubClone
+//
+//  Created by Mohamed Sayed on 28.05.24.
+//
+
+import UIKit
+
+class RepositoryCell: UITableViewCell {
+    
+    
+    
+    
+    func configure(fromRepository: Repository) {
+        
+    }
+}
