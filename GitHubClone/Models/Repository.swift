481301fa5330f5e3f4d7914f3b37dc0abaf4a121
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 14:38:22 +0200

    Create Repository Model

--- GitHubClone/Models/Repository.swift
+++ GitHubClone/Models/Repository.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct Repository: Decodable {
+    let name: String
+    let owner: Owner
+    let description: String?
+    let license: License?
+}
+
+struct Owner: Decodable {
+    let login: String
+}
+
+struct License: Decodable {
+    let name: String
+}
