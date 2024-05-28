956952b7a3eaeb62871451d36e2797791c881b80
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 08:00:38 +0200

    Create UsersCell

--- GitHubClone/UsersView/UsersCell/UserCell.swift
+++ GitHubClone/UsersView/UsersCell/UserCell.swift
@@ -8,19 +8,39 @@
 import UIKit
 
 final class UserCell: UITableViewCell {
+    private let userCellViewModel = UserCellViewModel(imageDownloader: ImageDownloader())
+    
     @IBOutlet weak private var userImage: UIImageView!
     @IBOutlet weak private var usernameLabel: UILabel!
     @IBOutlet weak private var repositoriesLabel: UILabel!
     @IBOutlet weak private var followersLabel: UILabel!
+    private var spinner = UIActivityIndicatorView()
     
     func configure(fromUser user: User) {
-//        userImage.layer.cornerRadius = userImage.frame.width / 2
-//        userImage.layer.borderWidth = 1
-//        userImage.layer.borderColor = UIColor.black.cgColor
- //       userImage.image = U
+        createSpinner(view: userImage)
+        
+        userCellViewModel.downloadImage(fromURL: user.avatarUrl) { [weak self] image in
+            guard let self else {
+                return
+            }
+            
+            self.userImage.layer.cornerRadius = userImage.frame.width / 2
+            self.userImage.layer.borderWidth = 1
+            self.userImage.layer.borderColor = UIColor.black.cgColor
+            self.userImage.image = UIImage(data: image)
+            
+            spinner.stopAnimating()
+        }
         
         usernameLabel.text = user.login
         repositoriesLabel.text = "Repositories: \(user.publicRepos)"
         followersLabel.text = "Followers: \(user.followers)"
+    }
+    
+    private func createSpinner(view: UIView) {
+        spinner.center = view.center
+        view.addSubview(spinner)
+        spinner.startAnimating()
+        spinner.hidesWhenStopped = true
     }
 }
