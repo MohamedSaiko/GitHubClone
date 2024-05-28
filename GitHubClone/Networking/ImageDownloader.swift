6489c5fedb8aec171bb5b279990f95ce7db8b907
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 08:03:58 +0200

    Create ImageDownloader

--- GitHubClone/Networking/ImageDownloader.swift
+++ GitHubClone/Networking/ImageDownloader.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+enum ImageDownloadError: Error {
+    case downloadingError
+    case noResponse
+}
+
+final class ImageDownloader {
+    func download(fromURL url: URL, completion: @escaping (Result<Data, ImageDownloadError>) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                completion(.failure(.downloadingError))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                  (200...299).contains(response.statusCode) else {
+                completion(.failure(.noResponse))
+                return
+            }
+            
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
