bb552f7e9a5efab9d90529dc48706468d9c4930c
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-28 16:27:09 +0200

    Create ForksViewController

--- GitHubClone/ForksView/ForksViewController.swift
+++ GitHubClone/ForksView/ForksViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class ForksViewController: UIViewController {
-
+    private let forksViewModel = ForksViewModel(networkManager: NetworkManager())
+    var repository = ""
+    var username = ""
+    
+    @IBOutlet private weak var forksTableView: UITableView!
+    private var spinner = UIActivityIndicatorView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        forksTableView.dataSource = self
+        
+        createSpinner()
+        updateTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateTableView() {
+        forksViewModel.getforks(withUsername: username, withRepository: repository) { [weak self] in
+            guard let self else {
+                return
+            }
+            
+            spinner.stopAnimating()
+            self.forksTableView.reloadData()
+        }
     }
-    */
+    
+    private func createSpinner() {
+        spinner.center = view.center
+        view.addSubview(spinner)
+        spinner.startAnimating()
+        spinner.hidesWhenStopped = true
+    }
+}
 
+//MARK: ForksTableViewDataSource
+
+extension ForksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return forksViewModel.forks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let forkCell = tableView.dequeueReusableCell(withIdentifier: CellsIdentifier.shared.forkCellID, for: indexPath) as? ForkCell
+        
+        guard let forkCell = forkCell else {
+            return UITableViewCell()
+        }
+        
+        forkCell.configure(fromFork: forksViewModel.forks[indexPath.row])
+        return forkCell
+    }
 }
