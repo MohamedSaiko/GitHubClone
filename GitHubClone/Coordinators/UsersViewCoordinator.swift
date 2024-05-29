29a4e96a81a27c54d0f5d23b7aa4c39bc3d471aa
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-29 13:04:40 +0200

    Create UsersViewCoordinator

--- GitHubClone/Coordinators/UsersViewCoordinator.swift
+++ GitHubClone/Coordinators/UsersViewCoordinator.swift
@@ -5,4 +5,35 @@
 //  Created by Mohamed Sayed on 29.05.24.
 //
 
-import Foundation
+import UIKit
+
+class UsersViewCoordinator: Coordinator {
+    weak var parentCoordinator: MainCoordinator?
+    
+    var children = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    private let storyboard = UIStoryboard.init(name: "Main", bundle: .main)
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let usersViewController = storyboard.instantiateViewController(withIdentifier: StoryboardIdentifier.shared.usersViewControllerID) as? UsersViewController
+        
+        guard let usersViewController = usersViewController else {
+            return
+        }
+        
+        let usersViewModel = UsersViewModel(networkManager: NetworkManager())
+        usersViewModel.coordinator = self
+        usersViewController.usersViewModel = usersViewModel
+        
+        navigationController.pushViewController(usersViewController, animated: true)
+    }
+    
+    func goToRepositoriesViewController(withUsername username: String) {
+        parentCoordinator?.goToRepositoriesViewController(withUsername: username)
+    }
+}
