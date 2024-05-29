a31684d0245c7e6c5dfeea25de591296f3444286
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-29 13:04:12 +0200

    Create ForksViewCoordinator

--- GitHubClone/Coordinators/ForksViewCoordinator.swift
+++ GitHubClone/Coordinators/ForksViewCoordinator.swift
@@ -4,5 +4,36 @@
 //
 //  Created by Mohamed Sayed on 29.05.24.
 //
+import UIKit
 
-import Foundation
+class ForksViewCoordinator: Coordinator {
+    weak var parentCoordinator: MainCoordinator?
+    
+    var children = [Coordinator]()
+    var navigationController: UINavigationController
+    private let storyboard = UIStoryboard.init(name: "Main", bundle: .main)
+    
+    var repository = ""
+    var username = ""
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let forksViewController = storyboard.instantiateViewController(withIdentifier: StoryboardIdentifier.shared.forksViewController) as? ForksViewController
+        
+        guard let forksViewController = forksViewController else {
+            return
+        }
+        
+        let forksViewModel = ForksViewModel(networkManager: NetworkManager())
+        forksViewModel.coordinator = self
+        forksViewController.forksViewModel = forksViewModel
+        
+        forksViewController.repository = repository
+        forksViewController.username = username
+        
+        navigationController.pushViewController(forksViewController, animated: true)
+    }
+}
