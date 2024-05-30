66dbefc07086b1aebcc295dbf35bb58162625925
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-30 22:56:27 +0200

    Create ForkCellViewModelTests and userCellViewModelTests

---
+++ GitHubCloneTests/Mocks/MockImageDownloader.swift
@@ -0,0 +1,21 @@
+//
+//  MockImageDownLoader.swift
+//  GitHubCloneTests
+//
+//  Created by Mohamed Sayed on 30.05.24.
+//
+
+import UIKit
+@testable import GitHubClone
+
+final class MockImageDownloader: AnyImageDownloader {
+    func download(fromURL url: URL, completion: @escaping (Result<Data, ImageDownloadError>) -> Void) {
+        let image = UIImage(named: "GitHubLogoImage")
+        guard let data = image?.pngData() else {
+            completion(.failure(.downloadingError))
+            return
+        }
+        
+        completion(.success(data))
+    }
+}
