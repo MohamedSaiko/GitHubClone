64696f2446345484425c39a2b12d61c38185b446
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-30 04:35:16 +0200

    Update ForksView in StoryBoard

---
+++ GitHubClone/ForksView/ForksCell/ForkCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ForkCellViewModel.swift
+//  GitHubClone
+//
+//  Created by Mohamed Sayed on 30.05.24.
+//
+
+import Foundation
---
+++ GitHubClone/Models/Owner.swift
@@ -0,0 +1,8 @@
+//
+//  Owner.swift
+//  GitHubClone
+//
+//  Created by Mohamed Sayed on 30.05.24.
+//
+
+import Foundation
