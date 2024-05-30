1884e36ecfd4c9dae02f045e2ce84f4de9a7e6a1
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-30 04:37:19 +0200

    Create ForkCellViewModel

--- GitHubClone/ForksView/ForksCell/ForkCellViewModel.swift
+++ GitHubClone/ForksView/ForksCell/ForkCellViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+
+final class ForkCellViewModel {
+    private let imageDownloader: ImageDownloader
+    private let imageCache: NSCache<NSURL, NSData>
+    
+    init(imageDownloader: ImageDownloader, imageCache: NSCache<NSURL, NSData>) {
+        self.imageDownloader = imageDownloader
+        self.imageCache = imageCache
+    }
+    
+    func downloadImage(fromURL url: URL, completion: @escaping (Data) -> Void) {
+        imageDownloader.download(fromURL: url) { [weak self] result in
+            guard let self else {
+                return
+            }
+            
+            switch result {
+            case .success(let image):
+                self.imageCache.setObject(image as NSData, forKey: url as NSURL)
+                
+                if let cachedImage = self.imageCache.object(forKey: url as NSURL) {
+                    DispatchQueue.main.async {
+                        completion(cachedImage as Data)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(image)
+                    }
+                }
+                
+            case .failure(_):
+                if let cachedImage = self.imageCache.object(forKey: url as NSURL) {
+                    DispatchQueue.main.async {
+                        completion(cachedImage as Data)
+                    }
+                }
+            }
+        }
+    }
+}
