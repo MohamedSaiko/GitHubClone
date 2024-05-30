62387eed45d3b33a6c650b86714d282da081c55b
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-05-30 04:37:47 +0200

    Update ForksViewModel

--- GitHubClone/ForksView/ForksViewModel.swift
+++ GitHubClone/ForksView/ForksViewModel.swift
@@ -13,7 +13,7 @@ final class ForksViewModel {
     
     private(set) var forks = [Fork]()
     
-    private var perPage = 5
+    private var perPage = 10
     
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
